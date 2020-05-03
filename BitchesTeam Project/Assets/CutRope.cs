e4141e941c7d317af3b1c484c132f84274a42b1a
Kenzi <denis.zotov.98@inbox.ru>
2020-05-03 14:17:08 +0400

    Was create Rope and Stone
    Были созданы 3 префаба:
    Камень
    Одна веревка, удерживающая камень.
    Две веревки, удерживающие камень.

---
+++ BitchesTeam Project/Assets/CutRope.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutRope : MonoBehaviour
+{
+    [SerializeField] private GameObject _firstel;
+    void FixedUpdate()
+    {
+        gameObject.transform.rotation = _firstel.transform.rotation;
+    }
+    void OnMouseDown()
+    {
+        transform.parent.gameObject.SetActive(false);
+    }
+}
