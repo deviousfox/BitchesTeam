fafb3b86093eaf7fa5677421f4bd523f7b7a6fff
Kenzi <denis.zotov.98@inbox.ru>
2020-05-05 23:57:35 +0400

    Liuid was created
    Жижидксоти , лава , вода, кислота. 
    Шейдеры и прочая херота

---
+++ BitchesTeam Project/Assets/Scripts/Gameplay/LiquidSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiquidSpawner : MonoBehaviour
+{
+    [Tooltip("Префаб жидкости")]
+    [SerializeField] private GameObject _goPref;
+    [Tooltip("Количество частиц жидкости")]
+    [SerializeField] private int _liquidQuantity;
+    void Start()
+    {
+        Spawner();
+    }
+
+    private void Spawner()
+    {
+        int quantity = 0;
+        while (quantity <= _liquidQuantity)
+        {
+            Vector3 spawnpoint = new Vector3(Random.Range(0.3f,0.7f), Random.Range(0.3f, 0.7f),0f);
+            Instantiate(_goPref,transform.position + spawnpoint, Quaternion.identity);
+            quantity++;
+        }
+    }
+}
