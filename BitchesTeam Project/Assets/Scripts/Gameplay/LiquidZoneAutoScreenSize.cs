037c95790b305d135cfad554dbc9a6c306134550
Kenzi <denis.zotov.98@inbox.ru>
2020-05-22 23:31:25 +0400

    xz

---
+++ BitchesTeam Project/Assets/Scripts/Gameplay/LiquidZoneAutoScreenSize.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiquidZoneAutoScreenSize : MonoBehaviour
+{
+    [Tooltip("Рендер текстура")]
+    [SerializeField] private RenderTexture _liquidRenderTexture;
+    void Awake()
+    {
+        _liquidRenderTexture.width = Screen.width;
+        _liquidRenderTexture.height = Screen.height;
+    }
+
+}
