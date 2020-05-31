76a90f3363e64e25c25924c819ab273faac43c3e
deviousfox <userMesh@yandex.ru>
2020-06-01 01:19:36 +0300

    Lockers is complite

--- BitchesTeam Project/Assets/Scripts/LockerComponentV4.cs
+++ BitchesTeam Project/Assets/Scripts/LockerComponentV4.cs
@@ -7,41 +7,103 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-class LockerComponentV4 : MonoBehaviour, IPointerClickHandler
+class LockerComponentV4 : MonoBehaviour
 {
+    public float MoveSpeed = 2f;
+    public SpriteRenderer LockerEar;
     public Transform StartTransform;
     public Transform EndTransform;
     public Transform LockerTransform;
     public LayerMask ObstacleMask;
 
-    public void OnPointerClick(PointerEventData eventData)
+    private void Start()
     {
-        if (MoveLocker() == null)
-        {
-            Vector2 direction = EndTransform.position - StartTransform.position;
-            RaycastHit2D[] raycastHits = Physics2D.RaycastAll(StartTransform.position, direction, ObstacleMask);
-            if (raycastHits.Length == 1)
-            {
-               if( raycastHits[0].transform == LockerTransform)
-               {
-                   StartCoroutine(MoveLocker());
-               }
-
-            }
-            else
-            {
-                //TODO Визуализация невозможности открытия.
-            }
-        }
+        LockerTransform.position = StartTransform.position;
     }
-    
+    public void OnMouseDown()
+    {
+        Vector2 direction = EndTransform.position - StartTransform.position;
+        RaycastHit2D[] raycastHits = Physics2D.RaycastAll(StartTransform.position, direction, ObstacleMask);
+        if (raycastHits.Length >= 1)
+        {
+            print(raycastHits.Length);
+
+            for (int i = 0; i <raycastHits.Length; i++)
+            {
+              print(  raycastHits[i].transform.name);
+            }
+            if (raycastHits[0].transform == transform)
+            {
+                print("J is"+LockersCounting(raycastHits));
+                if (LockersCounting(raycastHits)<=1)
+                {
+                    StartCoroutine(MoveLocker());
+                }
+                else
+                {
+                    StartCoroutine(LockIndication());
+                }
+            }
+
+        }
+        else if (raycastHits.Length == 0)
+        {
+            StartCoroutine(MoveLocker());
+        }
+        else
+        {
+            StartCoroutine(LockIndication());
+        }
+
+    }
+
     IEnumerator MoveLocker()
     {
+        print("Coroutine is START");
         while (LockerTransform.position != EndTransform.position) // Возможно придется поиграться с условием
         {
-
+            LockerTransform.position = Vector3.Lerp(LockerTransform.position, EndTransform.position, MoveSpeed * Time.deltaTime);
             yield return null;
         }
+        yield break;
     }
+    IEnumerator LockIndication()
+    {
+        float indicationTime = 5f;
+        Color tempColor = LockerEar.color;
+
+        while (indicationTime >0)
+        {
+            
+            indicationTime -= Time.deltaTime;
+            Color indicationColor = Color.Lerp(tempColor, Color.red, Mathf.PingPong(Time.time, 1));
+            LockerEar.color = indicationColor;
+            
+            yield return null;
+        }
+        LockerEar.color = tempColor;
+        yield break;
+    }
+
+    private int LockersCounting(RaycastHit2D[] arr)
+    {
+        int j = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i].collider.CompareTag("Stick"))
+            {
+                j++;
+            }
+        }
+        return j;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(StartTransform.position, EndTransform.position - StartTransform.position);
+    }
+
+
 }
+
 
